8d76bf3dbc88640c2b986fb1e41152ca73aafdc3
LuukG137 <luukgout@outlook.com>
2020-11-27 08:40:00 +0100

    Update ToggleDebug.h
    Cleaned code and added HIDE_EXTRA_INFO option

--- ToggleDebug.h
+++ ToggleDebug.h
@@ -7,14 +7,6 @@
 #define DEBUG 0
 #endif
 
-#define TDEBUG_PRINT(inst, x)
-#define DEBUG_BEGIN(x)
-#define DEBUG_PRINT(x)
-#define DEBUG_PRINTLN(x)
-#define TDebug(name, delay)
-#define availableMem(...)
-#define DEBUG_FLUSH()
-
 #define FILE_NAME (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1: __FILE__)
 
 #ifdef FILE_SHOW_FULL_PATH
@@ -23,36 +15,42 @@
 
 #endif
 
+#define GETMEM(inst, errfunc, infoMode, ...) if(inst.getAvailableMemory(FILE_NAME, __PRETTY_FUNCTION__, __LINE__, infoMode)) { errfunc; }
+#define DEFAULT ; //Default second argument for availableMem()
+#define SHOW_INFO true
+#define EXTRA_INFO String(millis()) + ": " + FILE_NAME + " " + __PRETTY_FUNCTION__ + " line:" + __LINE__ + " "
 
-#define GETMEM(inst, errfunc, ...) if(inst.getAvailableMemory(FILE_NAME, __PRETTY_FUNCTION__, __LINE__)) { errfunc; }
-#define DEFAULT ;
-
-#if (DEBUG == 1) //Standard debug mode
-
-#define TDEBUG_PRINT(inst, x) inst.debugPrint(String(millis()) + ": " + FILE_NAME + " " + __PRETTY_FUNCTION__ + " line:" + __LINE__ + " " + String(x)) //Add useful information to debug message
+//Standard debug mode 1
+#define TDEBUG_PRINT(inst, x) inst.debugPrint(EXTRA_INFO + String(x)) //Add useful information to debug message
 #define DEBUG_BEGIN(x) Serial.begin(x)
 #define DEBUG_PRINT(x) Serial.print(String(x))
-#define DEBUG_PRINTLN(x) Serial.println(String(millis()) + ": " + FILE_NAME + " " + __PRETTY_FUNCTION__ + " line:" + __LINE__ + " " + String(x))
-#define TDebug(name, delay) ToggleDebug name(delay)
-#define availableMem(...) GETMEM(__VA_ARGS__, ,##__VA_ARGS__ DEFAULT )
+#define DEBUG_PRINTLN(x) Serial.println(EXTRA_INFO + String(x))
+#define TDebug(inst, delay) ToggleDebug inst(delay)
+#define availableMem(...) GETMEM(__VA_ARGS__, SHOW_INFO, ,##__VA_ARGS__ DEFAULT)
 #define DEBUG_FLUSH() Serial.flush()
+
+#if defined(HIDE_EXTRA_INFO) //String only mode
+
+#define SHOW_INFO false
+#define EXTRA_INFO ""
+
+#endif
+
+#if (DEBUG == 0) //No debugging, nothing will be compiled
+
+#define TDEBUG_PRINT(inst, x)
+#define DEBUG_BEGIN(x)
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#define TDebug(inst, delay)
+#define availableMem(...)
+#define DEBUG_FLUSH()
 
 #elif (DEBUG == 2) //Ram only mode
 
-#define DEBUG_BEGIN(x) Serial.begin(x)
-#define TDebug(name, delay) ToggleDebug name(delay)
-#define availableMem(...) GETMEM(__VA_ARGS__, ,##__VA_ARGS__ DEFAULT )
-#define DEBUG_FLUSH() Serial.flush()
-
-#elif (DEBUG == 3) //String only mode
-
-#define TDEBUG_PRINT(inst, x) inst.debugPrint(x)
-#define DEBUG_BEGIN(x) Serial.begin(String(x))
-#define DEBUG_PRINT(x) Serial.print(String(x))
-#define DEBUG_PRINTLN(x) Serial.println(String(x))
-#define TDebug(name, delay) ToggleDebug name(delay)
-#define availableMem(...) GETMEM(__VA_ARGS__, ,##__VA_ARGS__ DEFAULT )
-#define DEBUG_FLUSH() Serial.flush()
+#define TDEBUG_PRINT(inst, x)
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
 
 #endif
 
@@ -61,7 +59,7 @@ class ToggleDebug
 public:
     ToggleDebug(int delay);
     void debugPrint(String message);
-    bool getAvailableMemory(String file, String method, int line);
+    bool getAvailableMemory(String file, String method, int line, bool extraInfo);
 
 private:
     unsigned long _timer;
@@ -69,5 +67,3 @@ private:
     int _prevMem;
 };
 #endif
-
-//Todo: Pass existing method in getAvailableMemory instead of using if statement
