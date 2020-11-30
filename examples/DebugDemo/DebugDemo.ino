92cd78aa147b3b2360c0509c52a8def4dfd321d5
LuukG137 <luukgout@outlook.com>
2020-11-30 11:03:09 +0100

    Update DebugDemo.ino
    Edited function names and added descriptions

--- examples/DebugDemo/DebugDemo.ino
+++ examples/DebugDemo/DebugDemo.ino
@@ -1,38 +1,43 @@
 /*
 #define DEBUG 0  *Debug mode off, none of the debug functions will be compiled.
 #define DEBUG 1  *Debug mode. All debug functions are included and useful information like current method, line and millis are printed along with the debug message.
-#define DEBUG 2  *Ram only mode. Only the freeRam function is compiled.
+#define DEBUG 2  *Ram only mode. Only the PRINTMEM functions are compiled.
 
 #define FILE_SHOW_FULL_PATH * This will show the full file path in stead of the file name.
 #define HIDE_EXTRA_INFO * This will hide extra info like filename, line and method from debug messages
 
-*Define DEBUG before including the library
-*DEBUG will be defined as 0 if not defined by the user
+*Define these options before including the library
+*DEBUG will be defined as 0 if not defined in the sketch
 
 DEBUG_BEGIN()   = Serial.begin()
 DEBUG_PRINT()   = Serial.print()
 DEBUG_PRINTLN() = Serial.println()
 DEBUG_FLUSH()   = Serial.flush()
 
-TDebug(timer, delay)  *Create debug timer instance. This timer is used for non blocking, timed printlns
+TDEBUG(instance, delay)  *Create debug timer instance. This timer is used for non-blocking, timed printlns
 TDEBUG_PRINT(timer instance, "Message")  *Print timed message every x milliseconds. Will show extra information like current millis, line and method if enabled
-availableMem(timer instance, error method (optional))  *Print timed message which shows ammount of RAM left. Calls optional method if change in memory is detected
+TDEBUG_PRINTMEM(timer instance, error method (optional))  *Print timed message which shows ammount of RAM left between stack and heap. Calls optional method if change in memory is detected
 */
 
 #define DEBUG 1 //Toggle debug messages by commenting or de-commenting this define
+//#define FILE_SHOW_FULL_PATH
+//#define HIDE_EXTRA_INFO
+
 #include <ToggleDebug.h>
 
-TDebug(timedMessage, 3000); //Initialize timed debug message
-TDebug(timedMessage2, 200);
+TDEBUG(timedMessage, 3000); //Create TDEBUG instance for use with TDEBUG_PRINT() method
+TDEBUG(timedMessage2, 200);
 
-TDebug(availableMemTimer, 6000); //Initialize timed debug message for availableMem() method
+TDEBUG(PRINTMEMTimer, 6000); //Create TDEBUG instance for TDEBUG_PRINTMEM() method
 
 void setup()
 {
     DEBUG_BEGIN(115200);
 
-    DEBUG_PRINTLN("Println");
+    DEBUG_PRINTLN("Println"); //These won't compile if DEBUG is set to 0
     DEBUG_PRINT("Print");
+
+    DEBUG_PRINTMEM(); //Prints and returns ammount of free memory between stack and heap.
 }
 
 void loop()
@@ -40,10 +45,11 @@ void loop()
     TDEBUG_PRINT(timedMessage, "This is a non blocking, timed and toggleable debug message");
     TDEBUG_PRINT(timedMessage2, ":)");
 
-    availableMem(availableMemTimer, errorMethod()) //Prints ammount of unused RAM between the stack and heap. Useful for finding memory leaks.
-}                                                  //Optional method can be passed that will fire when a memory error is detected.
+    TDEBUG_PRINTMEM(PRINTMEMTimer, errorMethod()); //Timed PRINTMEM method. Calls optional method when change in memory has been detected.
+  //TDEBUG_PRINTMEM(availableMemTimer); //Also works
+}
 
 void errorMethod()
 {
-    DEBUG_PRINTLN("Prints if memory error is detected by availableMem method");
+    DEBUG_PRINTLN("Prints if memory change is detected by TPRINTMEM method");
 }
